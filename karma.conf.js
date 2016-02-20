39ce36c6e30212b1e2e1d4c231673cb28e73cf6d
Floyd Pink <imbleedingme@googlemail.com>
2016-02-19 23:56:26 -0500

    fixes #1 - karma.conf.js had a files section that was not including all of the required files

--- karma.conf.js
+++ karma.conf.js
@@ -15,10 +15,16 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/jasmine-core/lib/jasmine-core/jasmine.js',
+      'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/services/*.js',
       'app/scripts/controllers/*.js',
