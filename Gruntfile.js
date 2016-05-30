b4652d09a2d0e98f0d576208bacb2cd18852fab6
Arun Kutty <kutty.a@gmail.com>
2016-05-30 11:50:25 +0530

    Fixed 404 for templates (modal html and tally xml)

--- Gruntfile.js
+++ Gruntfile.js
@@ -380,7 +380,8 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '*.html',
             'images/{,*/}*.{webp}',
-            'styles/fonts/{,*/}*.*'
+            'styles/fonts/{,*/}*.*',
+            'templates/*.xml' //Tally template files
           ]
         }, {
           expand: true,
